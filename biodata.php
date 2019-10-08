bae614129ac19255dd667281e7885a76409a2514
Vidya1771100006 <56057785+Vidya1771100006@users.noreply.github.com>
2019-10-08 09:50:26 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,9 @@
+<?php
+print "BIODATA</br>";
+print "=========================================</br>";
+print "Heloo, saya Vidya Eka Jayanti.</br>";
+print "Saya mahasiswa.</br>";
+print "Mahasiswa di UNWIDHA.</br>";
+print "Saya mengambil jurusan Teknik Informatika.</br>";
+print "Saya sekarang semester 5.</br>";
+?>
